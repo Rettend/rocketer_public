9332936b48291d0f47b57e7cd5f9cd65a87932d6
Rettend <36823200+Rettend@users.noreply.github.com>
2018-06-10 19:09:05 +0200

    Update Rocketer_Public.py

--- Rocketer_Public.py
+++ Rocketer_Public.py
@@ -414,7 +414,7 @@ async def on_message(message):
                            ":small_blue_diamond: Type `r--latest` to get the latest updates!\n"
                            f":white_small_square: If you have any questions, ask it to {Rettend.mention}", colour=0x3498db)
         em.set_thumbnail(url="https://cdn.discordapp.com/emojis/430347128100093962.gif?v=1")
-        await bot.send_message(message.channel, embed=em)   
+        await bot.send_message(message.channel, embed=em)
 	if message.content.startswith('r--bot'):
         em = discord.Embed(description= "```md\n"
                                 "<⊐______⊐______⊏THE-ROCKETER-BOT⊐______⊏______⊏>\n"
