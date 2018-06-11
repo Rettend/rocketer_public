3fa0ac0de64b0980259780088a9e73eb91779424
Rettend <36823200+Rettend@users.noreply.github.com>
2018-06-11 15:51:47 +0200

    Update members.py

--- members.py
+++ members.py
@@ -23,23 +23,24 @@ class Members():
     async def joined(self, member : discord.Member):
         await self.bot.say('**{0.name} joined in __{0.joined_at}__**'.format(member))
     
-    @bot.event
-    async def on_message(message):
-        if message.content.startswith("r--set_welcome"):
-            wchannel = message.channel
-            emb = discord.Embed(title="DONE!", description=f":blue_book: Welcome-message set to this channel!", colour=0x3498db)
-            emb.set_thumbnail(url="https://discordapp.com/assets/c6b26ba81f44b0c43697852e1e1d1420.svg")
-            await bot.say(embed=emb)
+    @commands.command(pass_context=True)
+    async def set_welcome(ctx, mesg1, mesg2):
+        wchannel = message.channel
+        emb = discord.Embed(title="DONE!", description=f":blue_book: Welcome-message set to this channel!\nWhen a member joins: **{mesg1}**\nWhen a member leaves: **{mesg2}**", colour=0x3498db)
+        emb.set_thumbnail(url="https://discordapp.com/assets/c6b26ba81f44b0c43697852e1e1d1420.svg")
+        await bot.say(embed=emb)
 
     @bot.event
     async def on_member_join(member):
-        em = discord.Embed(title=f"{member.metion} joined!", description="Welcome, have a great time here!  :D", colour=0x3498db)
+        em = discord.Embed(title=f"{member.metion} joined!", description="mesg1", colour=0x3498db)
         em.set_thumbnail(url="https://cdn.discordapp.com/emojis/391322023739129856.png?v=1")
         await bot.send_message(wchannel, embed=em)
         
     @bot.event
     async def on_member_remove(member):
-        await bot.send_message(wchannel, f"**{member} left without saying anything...** <:thonkSad:421004865049985035>")
+        em = discord.Embed(title=f"{member.metion} joined!", description="mesg2", colour=0x3498db)
+        em.set_thumbnail(url="https://cdn.discordapp.com/emojis/421004865049985035.png?v=1")
+        await bot.send_message(wchannel, embed=embed)
     
 def setup(bot):
     bot.add_cog(Members(bot))
