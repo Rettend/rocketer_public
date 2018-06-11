6c6d604baec93f4db8ede402e4c7521138bdb89a
Rettend <36823200+Rettend@users.noreply.github.com>
2018-06-11 15:54:35 +0200

    Update members.py

--- members.py
+++ members.py
@@ -25,7 +25,7 @@ class Members():
     
     @commands.command(pass_context=True)
     async def set_welcome(ctx, mesg1, mesg2):
-        wchannel = message.channel
+        wchannel = ctx.message.channel
         emb = discord.Embed(title="DONE!", description=f":blue_book: Welcome-message set to this channel!\nWhen a member joins: **{mesg1}**\nWhen a member leaves: **{mesg2}**", colour=0x3498db)
         emb.set_thumbnail(url="https://discordapp.com/assets/c6b26ba81f44b0c43697852e1e1d1420.svg")
         await bot.say(embed=emb)
@@ -38,8 +38,8 @@ class Members():
         
     @bot.event
     async def on_member_remove(member):
-        em = discord.Embed(title=f"{member.metion} joined!", description="mesg2", colour=0x3498db)
-        em.set_thumbnail(url="https://cdn.discordapp.com/emojis/421004865049985035.png?v=1")
+        embed = discord.Embed(title=f"{member.metion} joined!", description="mesg2", colour=0x3498db)
+        embed.set_thumbnail(url="https://cdn.discordapp.com/emojis/421004865049985035.png?v=1")
         await bot.send_message(wchannel, embed=embed)
     
 def setup(bot):
