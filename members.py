36657db86192053dc83482e9c75246fbd1ed45e1
Rettend <36823200+Rettend@users.noreply.github.com>
2018-06-19 16:24:39 +0200

    Update members.py

--- members.py
+++ members.py
@@ -7,20 +7,20 @@ class Members():
         await self.bot.say('**{0.name} joined in __{0.joined_at}__**'.format(member))
     
     @commands.command(pass_context=True)
-    async def set_welcome(ctx, mesg1, mesg2):
+    async def set_welcome(self, ctx, mesg1, mesg2):
         wchannel = ctx.message.channel
         emb = discord.Embed(title="DONE!", description=f":blue_book: Welcome-message set to this channel!\nWhen a member joins: **{mesg1}**\nWhen a member leaves: **{mesg2}**", colour=0x3498db)
         emb.set_thumbnail(url="https://discordapp.com/assets/c6b26ba81f44b0c43697852e1e1d1420.svg")
         await bot.say(embed=emb)
 
     @bot.event
-    async def on_member_join(member):
+    async def on_member_join(self, member):
         em = discord.Embed(title=f"{member.metion} joined!", description="mesg1", colour=0x3498db)
         em.set_thumbnail(url="https://cdn.discordapp.com/emojis/391322023739129856.png?v=1")
         await bot.send_message(wchannel, embed=em)
         
     @bot.event
-    async def on_member_remove(member):
+    async def on_member_remove(self, member):
         embed = discord.Embed(title=f"{member.metion} joined!", description="mesg2", colour=0x3498db)
         embed.set_thumbnail(url="https://cdn.discordapp.com/emojis/421004865049985035.png?v=1")
         await bot.send_message(wchannel, embed=embed)
