a0208d13059376a4ddeded29acb4c6a1df6ef399
Rettend <36823200+Rettend@users.noreply.github.com>
2018-06-19 14:35:30 +0200

    Update Rocketer_Public.py

--- Rocketer_Public.py
+++ Rocketer_Public.py
@@ -69,7 +69,7 @@ async def clear(ctx, number : int):
         msg = await bot.send_message(ctx.message.channel, embed=em)
         await asyncio.sleep(4)
         await bot.delete_message(msg)
-    if perm.manage_messages or perm.administrator == False:
+    if perm.manage_messages == False:
         await bot.send_message(ctx.message.channel, f'*Boi, you cant use this command...*')
         raise NoPermError
 
