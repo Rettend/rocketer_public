2e3122b359f01b72846098778ece21f252ec7df9
Rettend <36823200+Rettend@users.noreply.github.com>
2018-06-19 16:11:55 +0200

    Update members.py

--- members.py
+++ members.py
@@ -1,20 +1,3 @@
-import discord, logging, json, asyncio, time, random, aiohttp, re, datetime, traceback, os, sys, math
-from discord.ext import commands
-
-#-------------------DATA---------------------
-version = "0.8.9"
-owner = ["361534796830081024"]
-startup_extensions = ["members", "rng"]
-bot = commands.Bot(command_prefix='r--', description=None)
-bot.remove_command("help")
-message = discord.Message
-server = discord.Server
-member = discord.Member
-user = discord.User
-Imox = ["365173881952272384"]
-permissions = discord.Permissions
-underworking = ":warning: **Meh Boi, this command hasn't finished. Please wait until it's got.** :warning:"
-
 class Members():
     def __init__(self, bot):
         self.bot = bot
