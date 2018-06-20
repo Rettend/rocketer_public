2eb6ef22c88a9a5d8320d5bcb8d83dc1d1b805ec
Rettend <36823200+Rettend@users.noreply.github.com>
2018-06-20 12:23:40 +0200

    Update moderation.py

--- moderation.py
+++ moderation.py
@@ -23,8 +23,9 @@ class Mod():
 
     @commands.command(pass_context=True)
     @commands.has_permissions(manage_server=True)
-    async def set_log(self, ctx, channel : discord.Channel):
-        LogRoom = channel
+    async def setlog(self, ctx, channel):
+        channel= f"{channel}"
+        LogRoom = discord.utils.get(ctx.message.server.channels, name=channel)
         emb = discord.Embed(title="DONE!", description=f":blue_book: Logging channel set to {LogRoom.mention}!\nThe next moderations will logged there!", colour=0x3498db)
         emb.set_thumbnail(url="https://discordapp.com/assets/c6b26ba81f44b0c43697852e1e1d1420.svg")
         await bot.say(embed=emb)
